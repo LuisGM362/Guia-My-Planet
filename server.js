0c0fae1a12e8ff9045a1185b59af5ede788cb094
LuisGM362 <hector.luis.ariza@gmail.com>
2025-08-09 18:08:14 -0300

    modiicar las opcciones

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/opciones', (req, res) => {
-  res.json(["Opción 1", "Opción 2", "Opción 3"]);
+  res.json(["Guia de inicio", "Guia de consulta", "Guia de victoria"]);
 });
 
 const PORT = 3001;
